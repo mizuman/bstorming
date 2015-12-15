2dca5d73f6db018da9dec792ee0d6d674a3c0695
mizuman <YoshikiMiz@gmail.com>
2015-12-15 12:07:05 +0900

    test for websocket

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
-var express = require("express")
-var app = express()
-var port = process.env.PORT || 5000
+var express = require("express");
+var app = express();
+var port = process.env.PORT || 5000;
 // var WebSocketServer = require("ws").Server
-var http = require("http")
-var server = http.createServer(app)
+var http = require("http");
+var server = http.createServer(app);
 
 var io = require("socket.io")(server);
 
-app.use(express.static(__dirname + "/"))
+app.use(express.static(__dirname + "/"));
 
-server.listen(port)
+server.listen(port);
 
-console.log("http server listening on %d", port)
+console.log("http server listening on %d", port);
 
-// var wss = new WebSocketServer({server: server})
-// console.log("websocket server created")
-
-// wss.on("connection", function(ws) {
-//   var id = setInterval(function() {
-//     ws.send(JSON.stringify(new Date()), function() {  })
-//   }, 1000)
-
-//   console.log("websocket connection open")
-
-//   ws.on("close", function() {
-//     console.log("websocket connection close")
-//     clearInterval(id)
-//   })
-// })
-
-io.on("connection", function(socket){
+var chat = io.sockets.on("connection", function(client){
 	console.log("connected");
 
-	socket.on("disconnect", function(){
+	client.emit("news", {msg:"hello world"});
+
+	client.on("msg", function(data){
+		console.log(data);
+	});
+
+	client.on("comment", function(data){
+		console.log(data);
+	});
+
+	client.on("disconnect", function(){
 		console.log("disconnected");
-	})
-})
+	});
+});
--- js/client.js
+++ js/client.js
@@ -3,3 +3,61 @@ var socket = io();
 socket.on('connect', function(){
     console.log("connected");
 });
+
+var socket = io.connect("/");
+
+function chat(room, name){
+	console.log("chat---" + room + name);
+	socket.on("connected" ,function(){
+		socket.json.emit("init", {"room": room, "name": name});
+	});
+
+	socket.on("message", function(data){
+		if(data){
+			update(data);
+		}
+	});
+
+	socket.on("news", function(data){
+		console.log(data);
+	})
+}
+
+function send(name) {
+	var data = $("#comment").val();
+	socket.json.send(data);
+	$("#comment").val("");
+}
+
+function update(data) {
+	// var $obj = $("<div>").text(data);
+	// $("#view").append($obj);
+	console.log(data);
+}
+
+(function(){
+	var room, name;
+	if(location.hash) {
+		room = location.hash.substr(1);
+	} else {
+		room = "public";
+	}
+
+	if($("#myid").val()) {
+		name = $("#myid").val();
+	} else {
+		name = Math.random().toString(36).slice(-8);
+	}
+
+	chat(room,name);
+
+	$("#comment").keypress(function(e){
+		if(e.which==13){
+			if($("#comment").val()){
+				var msg = $("#comment").val();
+				console.log(msg);
+				socket.json.emit("comment", {"room": room, "name": name, "msg": msg});
+			}
+		}
+	});
+})();
